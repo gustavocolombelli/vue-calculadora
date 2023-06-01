b0fb095e7e5efdcb6e0d703b3b278d96f11388b9
Gustavo Colombelli <gustavocolombelli@gmail.com>
2023-06-01 17:15:25 -0300

    att calc component

--- src/components/CalculadoraComponent.vue
+++ src/components/CalculadoraComponent.vue
@@ -1,67 +1,119 @@
-  <template>
+<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <table class="table table-bordered">
-      <tbody>
-        <tr>
-          <td colspan="4">{{ output || 0 }}</td>
-        </tr>
-        <tr>
-          <td v-on:click="clearField">C</td>
-          <td>+/-</td>
-          <td @click...>%</td>
-          <td class="right-column">/</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td class="right-column">x</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td class="right-column">-</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td class="right-column">+</td>
-        </tr>
-        <tr>
-          <td colspan="2">0</td>
-          <td>.</td>
-          <td class="right-column">=</td>
-        </tr>
-      </tbody>
-    </table>  
+    <div class="col-md-4 m-3">
+        <table class="table table-bordered">
+          <tbody>
+            <tr class="output">
+              <td colspan="4">
+                {{ output || 0 }}
+              </td>
+            </tr>
+            <tr>
+              <td v-on:click="clearField">C</td>
+              <td v-on:click="setNegativeOrPositive">+/-</td>
+              <td v-on:click="calculatePercentage">%</td>
+              <td v-on:click="processOutput('divide')" class="right-column">/</td>
+            </tr>
+
+            <tr>
+              <td v-on:click="getNumber('7')">7</td>
+              <td v-on:click="getNumber('8')">8</td>
+              <td v-on:click="getNumber('9')">9</td>
+              <td v-on:click="processOutput('multiply')" class="right-column">x</td>
+            </tr>
+            <tr>
+              <td v-on:click="getNumber('4')">4</td>
+              <td v-on:click="getNumber('5')">5</td>
+              <td v-on:click="getNumber('6')">6</td>
+              <td v-on:click="processOutput('subtract')" class="right-column">-</td>
+            </tr>
+            <tr>
+              <td v-on:click="getNumber('1')">1</td>
+              <td v-on:click="getNumber('2')">2</td>
+              <td v-on:click="getNumber('3')">3</td>
+              <td v-on:click="processOutput('add')" class="right-column">+</td>
+            </tr>
+            <tr>
+              <td v-on:click="getNumber('0')" colspan="2">0</td>
+              <td v-on:click="getDot()">.</td>
+              <td v-on:click="updateOutput" class="right-column">=</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CalculadoraComponent',
+  name: 'CalculatorComponent',
   props: {
     msg: String
   },
   data(){
     return{
-      output:'33'
-    }  
+      output:'0',
+      previousValue:null,
+      operationFired:false
+    }
   },
   methods:{
     clearField(){
-      this.output = '0'
+      this.output = '0';
+    },
+    setNegativeOrPositive(){
+      this.output = this.output[0] === '-'? this.output.slice(1):`-${this.output}`;
     },
     calculatePercentage(){
+      this.output = parseFloat(this.output)/100
+    },
+    getNumber(number){
+      if(this.operationFired){
+        this.output = '';
+        this.operationFired=false;
+      }
+      this.output=`${this.output}${number}`
+    },
+    getDot(){
+      if(this.output.indexOf('.') === -1){
+        this.output = this.output+'.'
+      }
+    },
+    processOutput(operation){
 
+      if(operation==='add'){
+        this.operation=(a, b)=>{
+          return parseFloat(a)+parseFloat(b);
+        }
+      }
+      else if(operation==='subtract'){
+        this.operation=(a, b)=>{
+          return parseFloat(a)-parseFloat(b);
+        }
+      }
+      if(operation==='divide'){
+        this.operation=(a, b)=>{
+          return parseFloat(a)/parseFloat(b);
+        }
+      }
+      if(operation==='multiply'){
+        this.operation=(a, b)=>{
+          return parseFloat(a)*parseFloat(b);
+        }
+      }
+      this.previousValue = this.output;
+      this.operationFired=true;
+    },
+    updateOutput(){
+      this.output= `${this.operation(this.previousValue, this.output)}`;
+      this.previousValue=null;
     }
+
   }
 }
 </script>
-  
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
@@ -79,13 +131,17 @@ a {
   color: #42b983;
 }
 
-.right-column{
-  background-color: blueviolet;
-  color:blue;
+.output{
+  background-color: #42b983;
+  color: aliceblue;
 }
 
-td:active{
+.right-column{
+  background-color: black;
+  color: aliceblue;
+}
+
+.right-column:active{
   background-color: grey;
 }
-
 </style>
